6bf8325e4483ea98ebb81b8e255b207a3a394e45
Mark Santoro <mark_santoro@comcast.net>
2024-08-25 09:14:57 -0700

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Mark Santoro on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncImage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AsyncImage
+//
+//  Created by Mark Santoro on 8/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
