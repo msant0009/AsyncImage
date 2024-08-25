f8ba16f4244c68734d617188d84c16a4f9be71ac
Mark Santoro <mark_santoro@comcast.net>
2024-08-25 09:56:42 -0700

    completed app

--- AsyncImage/ContentView.swift
+++ AsyncImage/ContentView.swift
@@ -9,15 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationView{
+            
+            VStack{
+                
+                AsyncImage(url: URL(string: "https://plus.unsplash.com/premium_photo-1661940781747-5b4026f9ee1f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")!) { image in
+                    image.resizable().frame(width: 300,height: 300,alignment: .center)
+                } placeholder: {
+                    ProgressView()
+                }
+                
+                List(superHeroArray) { superhero in
+                    Text(superhero.name)
+                        .font(.title3)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    
+                }
+                }.navigationTitle(Text("Superhero Book"))
+                
+            }
         }
-        .padding()
-    }
+    
+       
 }
+
 
 #Preview {
     ContentView()
